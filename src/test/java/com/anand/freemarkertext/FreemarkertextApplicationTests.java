41384dcfa2fbf8f59f37153cf2a0d99ae2cdaa7d
pursuitanand <pursuitanand@gmail.com>
2026-02-03 20:46:28 -0600

    Final Running Example

---
+++ src/test/java/com/anand/freemarkertext/FreemarkertextApplicationTests.java
@@ -0,0 +1,15 @@
+package com.anand.freemarkertext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreemarkertextApplicationTests {
+/*
+
+	@Test
+	void contextLoads() {
+	}
+*/
+
+}
